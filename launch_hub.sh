f9319d9764d52d3c3f7b54c03918ea5d49107f59
Corey <cdavis24@my.ccri.edu>
2025-07-12 22:11:41 -0400

    Delete launch_hub.sh

--- launch_hub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# launch_hub.sh - dynamic launcher for CCRI CTF
-
-# Resolve directory this script is in
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Call start_web_hub.sh relative to this location
-"$SCRIPT_DIR/stemday2025/web_version/start_web_hub.sh"
